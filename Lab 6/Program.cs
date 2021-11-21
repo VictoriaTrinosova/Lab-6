85a79c64454c61d589833a93eea07a75e95a4805
Виктория <@LAPTOP-CFFC9F7Q>
2021-11-21 14:54:39 +0300

    Добавьте файлы проекта.

---
+++ Lab 6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s=Console.ReadLine();
+            string[] stringArray = s.Split();
+            int maxlen = 0, index = 0;
+            for (int i=0; i<stringArray.Length; i++)
+            {
+                if (stringArray[i].Length > maxlen)
+                {
+                    maxlen = stringArray[i].Length;
+                    index = i;
+                    
+                }
+                
+            }
+            Console.Write("Самое длинное слово: {0}", stringArray[index]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab6.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string s2 = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                s2 += s[i];
+            }
+            if (s == s2)
+            {
+                Console.WriteLine("Фраза является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Фраза не является палиндромом");
+                Console.WriteLine(s2);
+                Console.ReadKey();
+            }
+
+
+
+        }
+    }
+}
